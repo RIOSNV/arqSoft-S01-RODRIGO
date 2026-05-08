9274f39a72c2a1f4d1af6aff597beb96efe55bba
RIOSNV <rr8972114@gmail.com>
2026-05-08 11:59:02 -0600

    depuracion del codigo y mejoras de interfaz

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,23 +1,21 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-document.addEventListener("DOMContentLoaded", () => {
+﻿document.addEventListener("DOMContentLoaded", () => {
     const links = document.querySelectorAll('a:not([target="_blank"]):not([href^="#"])');
     const content = document.getElementById('page-content');
 
-    if (content) { // Verificamos que el elemento exista
+    if (content) {
         links.forEach(link => {
             link.addEventListener('click', e => {
-                e.preventDefault();
-                const targetUrl = link.href;
+                if (link.hostname === window.location.hostname) {
+                    e.preventDefault();
+                    const targetUrl = link.href;
 
-                content.classList.remove('slide-in');
-                content.classList.add('slide-out');
+                    content.classList.remove('slide-in');
+                    content.classList.add('slide-out');
 
-                setTimeout(() => {
-                    window.location.href = targetUrl;
-                }, 500);
+                    setTimeout(() => {
+                        window.location.href = targetUrl;
+                    }, 500);
+                }
             });
         });
     }

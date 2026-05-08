2fc22ae7a95c2e1cdf3e87778cc2d8953fd9aae9
RIOSNV <rr8972114@gmail.com>
2026-05-08 11:06:52 -0600

    animaciones

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('a:not([target="_blank"]):not([href^="#"])');
+    const content = document.getElementById('page-content');
+
+    if (content) { // Verificamos que el elemento exista
+        links.forEach(link => {
+            link.addEventListener('click', e => {
+                e.preventDefault();
+                const targetUrl = link.href;
+
+                content.classList.remove('slide-in');
+                content.classList.add('slide-out');
+
+                setTimeout(() => {
+                    window.location.href = targetUrl;
+                }, 500);
+            });
+        });
+    }
+});
